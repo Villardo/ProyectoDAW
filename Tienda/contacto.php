e3b703ccf46ab8ad06f43585ab2996a22fd6cb48
Hadrián Villar <hadrianvillarcuadrado@gmail.com>
2021-06-19 05:19:41 +0200

    mapa goole maps y contacto

--- Tienda/contacto.php
+++ Tienda/contacto.php
@@ -14,7 +14,46 @@
 
     <!-- imagen -->
     <div>
-        <img src="images/foto1.jpg" class="imagen-completa">
+        <img src="images/foto3.jpg" class="imagen-completa">
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-6">
+                <form class="mt-3">
+                    <div class="row">
+                        <div class="col-3">
+                            <input type="text" class="form-control" id="contacto_nombre" name="usuario_nombre" placeholder="Nombre">
+                        </div>
+                        <div class="col-3">
+                            <input type="tel" class="form-control" id="contacto_telf" name="usuario_nombre" placeholder="Teléfono (opcional)">
+                        </div>
+                        <div class="col-6">
+                            <input type="email" class="form-control" id="contacto_email" name="usuario_nombre" placeholder="Email">
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <textarea id="txtid" class="form-control" name="txtname" rows="4" cols="50" maxlength="200"></textarea>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-6">
+                            <div class="form-group form-check">
+                                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                                <label class="form-check-label" for="exampleCheck1">Acepto enviar mis datos...</label>
+                            </div>
+                        </div>
+                        <div class="col-6 text-right">
+                            <button type="submit" class="btn btn-primary">Enviar</button>
+                        </div>
+                    </div>
+
+                </form>
+            </div>
+            <div class="col-6">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2955.054024115711!2d-8.695343184570465!3d42.21328997919706!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd25882f887b0879%3A0x1860e2dcd7c15677!2sColexio%20de%20Fomento%20Montecastelo!5e0!3m2!1ses!2ses!4v1624072717570!5m2!1ses!2ses" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+            </div>
+        </div>
     </div>
     <?php include 'footer.php' ?>
 </body>
